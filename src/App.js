07d1bb310ada97ec9e78cdc68e1fe693a4d9caff
AkshayMMurthy <138966239+AkshayMMurthy@users.noreply.github.com>
2023-10-16 04:09:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const wordCount = inputText.trim() === '' ? 0 : inputText.trim().split(/\s+/).length;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <br></br>
+      <br></br>
+      <textarea
+        
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      <br></br>
+      <p>Word Count: {wordCount}</p>
     </div>
   );
 }
